24b050e23e596f6d2ba0ae0d495d76c2c5b8a4d4
Leonardo Sanches <leonardopereirajr@gmail.com>
2022-05-21 18:37:44 -0300

    find by department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,8 +16,14 @@ public class Program {
 		System.out.println("=== Test 1: Seller findById ===");
 
 		Seller seller = sellerDao.findById(3);
-
 		System.out.println(seller);
+
+		System.out.println("\n=== Test 2: Seller findByDepartment ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -88,6 +91,41 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		return null;
 
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();// criado o map vazio e guardar o departamento que esta vindo
+
+			while (rs.next()) {
+
+				Department dep = map.get(rs.getInt("DepartmentId"));// no map eu busco o department Id que esta vindo e
+																	// verifico se ja existe
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
